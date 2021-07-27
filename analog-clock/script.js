c12b5cf604125fd28f97a0b6380ab93467af6d55
Dayana Mariano <dayanaraquelle@gmail.com>
2021-07-27 16:54:08 -0300

    Add analog-clock

---
+++ analog-clock/script.js
@@ -0,0 +1,30 @@
+//Pegar os elementos
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+//atualizar o relógio a cada segundo
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    //Relógio digital
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
+}
+
+function fixZero(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateClock, 1000);
